6c19c3230109343caa257549cac9fd64862ecc54
PB <lindynetech@gmail.com>
2025-11-27 20:40:30 -0500

    Add Github repo link to the footer

--- resources/views/includes/footerlanding.blade.php
+++ resources/views/includes/footerlanding.blade.php
@@ -2,9 +2,10 @@
 	<div class="container">
 		<div class="row text-center">
 			<div class="col-md-12">
-				
+
 				<ul class="footer-links inline-list" style="list-style-type:circle;">
 					<li>{!! config('tms.copyright') !!}</li>
+					<li><a href="https://github.com/lindynetech/tms" target="_blank" title="View on GitHub"><i class="fab fa-github"></i> GitHub</a></li>
 					<li><a href="{{ url('/privacypolicy') }}"><i class="fa fa-paperclip"></i> Privacy Policy</a></li>
 					<li><a href="{{ url('/#home') }}" class="nav-item"><i class="fa fa-pencil-alt"></i> Sign Up</a></li>
 					<li><a href="{{ url('/login') }}"><i class="fa fa-sign-in-alt"></i> Sign In</a></li>
